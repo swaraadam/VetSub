c3421d7c4bd8b8278a4fc403fd9f29c1e4423143
muhibush <ukiacrew@gmail.com>
2018-08-04 05:52:02 +0700

    login layout

---
+++ app/src/main/java/muhammad/shulhi/muhibush/vetsub/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package muhammad.shulhi.muhibush.vetsub.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import muhammad.shulhi.muhibush.vetsub.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
